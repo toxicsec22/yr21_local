5d9e153a2faca7728be768182621d6bf51f9e8bc
jyeusebio <jyeusebio@1rotarytrading.com>
2021-05-07 16:01:38 +0800

    used $txnidname consistently

--- acctg/formcv.php
+++ acctg/formcv.php
@@ -15,7 +15,7 @@ if (!allowedToOpen($lookupallow,'1rtc')) { echo 'No permission'; exit;}
 ?><br><div id="section" style="display: block;"><?php
 $file=basename($_SERVER['SCRIPT_FILENAME']);
 $w=(!isset($_GET['w'])?'List':$_GET['w']);
-$txnid='CVNo'; $txnidname='CVNo'; $form='CV'; $postfield='Posted';
+$txnidname='CVNo'; $form='CV'; $postfield='Posted';
 include_once $path.'/acrossyrs/commonfunctions/listoptions.php';
 
 $table='acctg_2cvmain'; $subtable='acctg_2cvsub'; 
