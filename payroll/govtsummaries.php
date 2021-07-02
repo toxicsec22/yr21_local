de18018c76f10ca7a63ea42f1c668a5b1b5f6c3d
markferrer1rotary <mark.ferrer@1rotarytrading.com>
2021-07-02 16:35:02 +0800

    added sss file export

--- payroll/govtsummaries.php
+++ payroll/govtsummaries.php
@@ -66,16 +66,25 @@ $exporton=0;
       case 'SSS':
          $reportname='SSS Summary for ';
          $cogovtno='SSSNo';
-         $sqlfirst='SELECT ss.*,b.Branch AS RecordInBranch,`SSS-EE`+`SSS-ERTotal`-EC-MPFER as SS_Amt, EC AS EC_Amt, MPFER AS MPFER_Amt, FORMAT(SSECMPFCredit,0) AS TotalSalaryCredit,`Company` FROM payroll_40sss ss JOIN 1companies c ON c.CompanyNo=ss.CompanyNo LEFT JOIN 1branches b ON ss.RecordInBranchNo=b.BranchNo WHERE PayrollID='.$_SESSION['payrollidses'];
+         $sqlfirst='SELECT ss.*,b.Branch AS RecordInBranch,`SSS-EE`+`SSS-ERTotal`-EC-MPFER as SS_Amt, IFNULL(EC,0) AS EC_Amt, MPFER AS MPFER_Amt,DATE_FORMAT(DateHiredResigned,"%m%d%Y") AS DateHiredResignedFormatted, DateHiredResigned,(SELECT (SELECT COUNT(IDNo) FROM payroll_40sss WHERE IDNo=ss.IDNo)+(SELECT COUNT(IDNo) FROM '.$lastyr.'_1rtc.payroll_40sss WHERE IDNo=ss.IDNo)) AS CountPayment,Resigned,
+         (CASE
+            WHEN (SELECT CountPayment)=1 THEN "Newly Hired"
+            WHEN Resigned=1 THEN "Resigned"
+            ELSE ""
          
+         END) AS SSRemarks,
+         FORMAT(SSECMPFCredit,0) AS TotalSalaryCredit,`Company` FROM payroll_40sss ss JOIN 1companies c ON c.CompanyNo=ss.CompanyNo LEFT JOIN 1branches b ON ss.RecordInBranchNo=b.BranchNo WHERE PayrollID='.$_SESSION['payrollidses'];
+      
 		 //removed EC and ss amt for easy view
          // $columnnames=array('SurName','FirstName','MI','RecordInBranch','SSSNo','SSS-EE','SSS-ERTotal','EC_Amt','SS_Amt','SSSTotal');
-         $columnnames=array('SurName','FirstName','MI','RecordInBranch','SSSNo','SSS-EE','SSS-ERTotal','SSSTotal','TotalSalaryCredit');
+         $columnnames=array('SurName','FirstName','MI','RecordInBranch','SSSNo','SSS-EE','SSS-ERTotal','SSSTotal','TotalSalaryCredit','SSRemarks');
        //  $sqlsumfirst='SELECT `Company`, Round(Sum(`SSS-EE`),2) as SSSEE,Round(Sum(`SSS-ER`),2) as SSSER,Sum(`SSSTotal`) as SSSTotal FROM payroll_40sss WHERE PayrollID='.$_SESSION['payrollidses'];
          $sqlsumfirst='SELECT `Company`, Round(Sum(`SSS-EE`+`SSS-ERTotal`-`EC`-MPFER),2) as SS_Amt, ROUND(Sum(`EC`),0) as EC_Amt, ROUND(Sum(`MPFER`),0) as MPFER_Amt,Sum(`SSSTotal`) as SSSTotal FROM payroll_40sss ss JOIN 1companies c ON c.CompanyNo=ss.CompanyNo WHERE PayrollID='.$_SESSION['payrollidses'];
          $columntotals=array('SS_Amt','EC_Amt','MPFER_Amt','SSSTotal');
 
-         $exportssstoform=1;
+         // $exportssstoform=1;
+         $exporton=1;
+
       break;
    case 'PHIC':
          $reportname='PHIC Summary for ';
@@ -153,7 +162,7 @@ $exporton=0;
      }
      $paydatesql='';
      if($exporton==1){
-         $paydatesql=',(SELECT CONCAT(YEAR(PayrollDate),LPAD(MONTH(PayrollDate),2,0)) FROM payroll_1paydates WHERE PayrollID='.$_SESSION['payrollidses'].') AS PayDate';
+         $paydatesql=',(SELECT CONCAT(YEAR(PayrollDate),LPAD(MONTH(PayrollDate),2,0)) FROM payroll_1paydates WHERE PayrollID='.$_SESSION['payrollidses'].') AS PayDateYYYYMM,(SELECT LPAD(MONTH(PayrollDate),2,0) FROM payroll_1paydates WHERE PayrollID='.$_SESSION['payrollidses'].') AS ApplicableMonth';
      }
 
 if (allowedToOpen(800,'1rtc')){ $sqlcompanies='SELECT *'.$paydatesql.' from `1companies` WHERE Active=1';}
@@ -168,7 +177,7 @@ $Company='';
 $Company=$co['Company'];
 $titlepercompany='<br>'.$reportname.$co['CompanyName'].'&nbsp &nbsp'.$cogovtno.':  '.$co[$cogovtno].'<br>';
 $sql=$sqlfirst.' and Company Like \''.$Company.'\'';
-//echo $sql;
+// echo $sql;
 $stmt=$link->query($sql);
 $result=$stmt->fetchAll();
 $colcount=count($columnnames)+1;
@@ -199,10 +208,58 @@ $records=$records+1;
 if($exporton==1){
    $piexportdetails='';
 
-   foreach($result as $rowexport){
-      $piexportdetails.=mb_str_pad('DT'.str_replace('-','',$rowexport['PagIbigNo']),'29','~',STR_PAD_RIGHT).mb_str_pad($rowexport['SurName'],30,'~',STR_PAD_RIGHT).mb_str_pad($rowexport['FirstName'],30,'~',STR_PAD_RIGHT).mb_str_pad($rowexport['MI'],30,'~',STR_PAD_RIGHT).mb_str_pad(number_format(($paytype=='MC'?$rowexport['PagIbig-EE']:$rowexport['PagibigLoan']),2),13,'0',STR_PAD_LEFT).mb_str_pad(number_format(($paytype=='MC'?$rowexport['PagIbig-ER']:0),2),13,'0',STR_PAD_LEFT).str_repeat('~',15).$rowexport['Bdate'].PHP_EOL;
+   //pagibig
+   if(in_array($whichqry,array('PagIbig','PagIbigLoans','PagIbigLoansCalamity'))){ 
+      foreach($result as $rowexport){
+         $piexportdetails.=mb_str_pad('DT'.str_replace('-','',$rowexport['PagIbigNo']),'29','~',STR_PAD_RIGHT).mb_str_pad($rowexport['SurName'],30,'~',STR_PAD_RIGHT).mb_str_pad($rowexport['FirstName'],30,'~',STR_PAD_RIGHT).mb_str_pad($rowexport['MI'],30,'~',STR_PAD_RIGHT).mb_str_pad(number_format(($paytype=='MC'?$rowexport['PagIbig-EE']:$rowexport['PagibigLoan']),2),13,'0',STR_PAD_LEFT).mb_str_pad(number_format(($paytype=='MC'?$rowexport['PagIbig-ER']:0),2),13,'0',STR_PAD_LEFT).str_repeat('~',15).$rowexport['Bdate'].PHP_EOL;
+      }
+      $piexportdetails=substr($piexportdetails, 0, -1);
    }
-   $piexportdetails=substr($piexportdetails, 0, -1);
+   //end pagibig
+
+   if(in_array($whichqry,array('SSS'))){ 
+      if($whichqry=='SSS'){
+         $gaid=1;
+      }
+      //check sbrno and other details
+      $sqlde='SELECT DATE_FORMAT(DatePaid,"%m%d%Y") AS DatePaid, DATE_FORMAT(CONCAT(LAST_DAY(CONCAT("'.$currentyr.'-",LPAD(ApplicableMonth,2,0),"-01"))),"%m%Y") AS ApplicableMY, RefNo,TotalAmount FROM payroll_1govtpaymentsinfo WHERE ApplicableMonth='.$co['ApplicableMonth'].' AND GAID='.$gaid.' AND CompanyNo='.$co['CompanyNo'].';';
+      // echo $sqlde;
+      $stmtde=$link->query($sqlde);
+      $resultde=$stmtde->fetch();
+
+      if($stmtde->rowCount()>0){
+      $totalsssee=0; $totalsssec=0;
+      foreach($result as $rowexport){
+         $piexportdetails.=mb_str_pad('20'.$rowexport['SurName'],'17','~',STR_PAD_RIGHT).mb_str_pad($rowexport['FirstName'],15,'~',STR_PAD_RIGHT).substr($rowexport['MI'],0,1).mb_str_pad(str_replace('-','',substr($rowexport['SSSNo'], 0, 10)),10,'~',STR_PAD_RIGHT).'~'.mb_str_pad(number_format(0,2),7,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format(($rowexport['SSSTotal']-$rowexport['EC_Amt']),2),7,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format(0,2),7,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format(0,2),5,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format(0,2),5,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format(0,2),5,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format(0,2),5,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format(0,2),5,$rowexport['EC_Amt'],STR_PAD_LEFT).'~'.mb_str_pad(number_format(0,2),5,'0',STR_PAD_LEFT).'~~~~~~';
+         
+         
+
+         if($rowexport['Resigned']==1){ //Resigned
+            $piexportdetails.='1'.mb_str_pad($rowexport['DateHiredResignedFormatted'],'8','~',STR_PAD_RIGHT);
+         } else { 
+
+           
+            
+            if($rowexport['CountPayment']==1){ //Newly Hired
+               $piexportdetails.='2'.mb_str_pad($rowexport['DateHiredResignedFormatted'],'8','~',STR_PAD_RIGHT);
+              } else {
+                $piexportdetails.='N'.mb_str_pad('0','8','~',STR_PAD_RIGHT);
+               }
+            }
+         //end condition
+
+         $piexportdetails.=PHP_EOL;
+         $totalsssee=$totalsssee+($rowexport['SSSTotal']-$rowexport['EC_Amt']);
+         $totalsssec=$totalsssec+$rowexport['EC_Amt'];
+      }
+     
+      //trailer
+      $piexportdetails.='99'.'~'.mb_str_pad(number_format(0,2),11,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format($totalsssee,2),11,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format(0,2),11,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format(0,2),9,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format(0,2),9,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format(0,2),9,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format(0,2),9,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format($totalsssec,2),9,'0',STR_PAD_LEFT).'~'.mb_str_pad(number_format(0,2),9,'0',STR_PAD_LEFT).mb_str_pad('~',20,'~',STR_PAD_RIGHT).PHP_EOL;
+   }
+      // $piexportdetails=substr($piexportdetails, 0, -1);
+
+   }
+
 }
     
    foreach($columntotals as $total){ 
@@ -212,16 +269,40 @@ if($exporton==1){
 
 $govtdata='<table>'.'<thead><tr><td colspan="'.$colcount.'">'.$titlepercompany.'</td></tr><tr>'.$tabletitle.'</tr></thead><tbody>'.$tabledata.'</tbody><tfoot><tr><td colspan="'.$colcount.'"> Totals '.str_repeat('&nbsp',5).$govttotals.'</td></tr></tfoot></table><br><br>';
 if($exporton==1){
-   //to change/
-   $piexportheader=strtoupper(mb_str_pad('EH'.mb_str_pad($co['PBranchCode'],2,0,STR_PAD_LEFT).''.$co['PayDate'].''.$co['PAgencyMemberNo'],25,'~',STR_PAD_RIGHT).mb_str_pad('P'.$paytype.''.$co['CompanyName'],103,'~',STR_PAD_RIGHT).mb_str_pad('HSS Tower 1 BGC Taguig',100,'~',STR_PAD_RIGHT).mb_str_pad('1634~~~77512213','22','~',STR_PAD_RIGHT)).PHP_EOL;
-   $filename='HMDF_'.$co['Company'].'_'.date('ymd').'_'.$paytype.'.mcl';
-   
 
-   $piexport=str_replace('~',' ',$piexportheader.$piexportdetails);
+   // start pagibig
+   if(in_array($whichqry,array('PagIbig','PagIbigLoans','PagIbigLoansCalamity'))){ 
+      $piexportheader=strtoupper(mb_str_pad('EH'.mb_str_pad($co['PBranchCode'],2,0,STR_PAD_LEFT).''.$co['PayDateYYYYMM'].''.$co['PAgencyMemberNo'],25,'~',STR_PAD_RIGHT).mb_str_pad('P'.$paytype.''.$co['CompanyName'],103,'~',STR_PAD_RIGHT).mb_str_pad('HSS Tower 1 BGC Taguig',100,'~',STR_PAD_RIGHT).mb_str_pad('1634~~~77512213','22','~',STR_PAD_RIGHT)).PHP_EOL;
+      $filename='HMDF_'.$co['Company'].'_'.date('ymd').'_'.$paytype.'.mcl';
+     
+      $piexport=str_replace('~',' ',$piexportheader.$piexportdetails);
+   }
+   //end pagibig
+   
+   // start sss
+   if(in_array($whichqry,array('SSS'))){ 
+  
+      
+      if($stmtde->rowCount()>0){
+         $piexportheader=strtoupper('00'.mb_str_pad(substr($co['GovtName'],0,30),30,'~',STR_PAD_RIGHT).$resultde['ApplicableMY'].$co[$cogovtno].mb_str_pad($resultde['RefNo'],10,'~',STR_PAD_RIGHT).$resultde['DatePaid'].mb_str_pad(number_format($resultde['TotalAmount'],2),12,'0',STR_PAD_LEFT)).PHP_EOL;
+
+         $filename='R3'.$co[$cogovtno].$resultde['ApplicableMY'].'.'.date('mdHi').'';
+        
+         // echo $piexportheader;
+         $piexport=str_replace('~',' ',$piexportheader.$piexportdetails);
+
+      }
+       
+   }
+   //end sss
+
+
    ?>
 <form style="display: inline" action='downloadpayrollfile.php' method='post'>
    <input type='submit' name='download' value='Download'>
+   <?php if(in_array($whichqry,array('PagIbig','PagIbigLoans','PagIbigLoansCalamity'))){ ?>
    <input type='hidden' name='fileext' value='mcl'>
+   <?php } ?>
    <input type='hidden' name='payrolldata' value='<?php echo $piexport; ?>'>
    <input type='hidden' name='filename' value='<?php echo $filename; ?>'>
 </form>
@@ -229,9 +310,9 @@ if($exporton==1){
    <?php
 }
 
-if($exportssstoform==1){
+// if($exportssstoform==1){
    
-}
+// }
 
 echo $govtdata;
 }
