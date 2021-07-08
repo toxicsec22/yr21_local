d43025f4660a9bad29d57a8123b66d9839c504d1
jyeusebio <jyeusebio@1rotarytrading.com>
2021-07-08 13:42:11 +0800

    separated treas & acctg for check vch.

--- backendphp/functions/editok.php
+++ backendphp/functions/editok.php
@@ -6,42 +6,43 @@ function editOk($table,$txnid,$linkinfunction,$txntype){
     } else { $closedate=$_SESSION['nb4']; }
     switch ($table){
         case 'invty_2transfer':
-            $sqlmain='Select s.`DateOUT`, s.`DateIN`,s.Posted, s.PostedIn, ToBranchNo, BranchNo from `'.$table.'`as s where TxnId='.$txnid;
+            $sqlmain='SELECT s.`DateOUT`, s.`DateIN`,s.Posted, s.PostedIn, ToBranchNo, BranchNo FROM `'.$table.'` s WHERE TxnId='.$txnid;
             break;
         case 'audit_2countmain':
         case 'audit_2toolscountmain':
         case 'audit_2countfreonmain':
         case 'audit_3vacuum':
-            $sqlmain='Select s.`Date`, s.Posted from `'.$table.'`as s where CountID='.$txnid;
+            $sqlmain='SELECT s.`Date`, s.Posted FROM `'.$table.'` s WHERE CountID='.$txnid;
             $date='Date';
             break;
         case 'audit_2countcash':
-            $sqlmain='Select s.`DateCounted`, s.Posted from `'.$table.'`as s where CashCountID='.$txnid;
+            $sqlmain='SELECT s.`DateCounted`, s.Posted FROM `'.$table.'` s WHERE CashCountID='.$txnid;
             $date='DateCounted';
             break;
         case 'acctg_2collectsubbounced':
-            $sqlmain='Select s.`DateBounced`, s.Posted from `'.$table.'`as s where TxnID='.$txnid;
+            $sqlmain='SELECT s.`DateBounced`, s.Posted FROM `'.$table.'` s WHERE TxnID='.$txnid;
             $date='DateBounced';
             break;
 		case 'acctg_3undepositedpdcfromlastperiodbounced':
-            $sqlmain='Select s.`DateBounced`, s.Posted from `'.$table.'`as s where UndepPDCId='.$txnid;
+            $sqlmain='SELECT s.`DateBounced`, s.Posted FROM `'.$table.'` s WHERE UndepPDCId='.$txnid;
             $date='DateBounced'; 
             break;
         case 'acctg_2cvmain':
-            $sqlmain='Select s.`Date`, s.Posted from `'.$table.'`as s where CVNo='.$txnid;
+        case 'acctg_4futurecvmain':
+            if(allowedToOpen(5432,'1rtc')){ // treasury
+                $sqlmain='SELECT s.`Date`, s.Posted AS `Posted` FROM `'.$table.'` s WHERE APVPosted=1 AND (CreditAccountID=403 OR CreditAccountID IN (SELECT AccountID FROM banktxns_1maintaining)) AND CVNo='.$txnid;
+            } else { //acctg
+                $sqlmain='SELECT s.`Date`, s.APVPosted AS `Posted` FROM `'.$table.'` s WHERE s.Posted=0 AND (CreditAccountID NOT IN (SELECT AccountID FROM banktxns_1maintaining)) AND CVNo='.$txnid;
+            }
             $date='Date';
             break;
         case 'acctg_2jvmain':
-            $sqlmain='Select s.`JVDate`, s.Posted from `'.$table.'`as s where JVNo='.$txnid;
+            $sqlmain='SELECT s.`JVDate`, s.Posted FROM `'.$table.'` s WHERE JVNo='.$txnid;
             $date='JVDate';
             break;
-		case 'acctg_4futurecvmain':
-            $sqlmain='Select s.`Date`, s.Posted from `'.$table.'`as s where CVNo='.$txnid;
-            $date='Date';
-            break;	
-			
+	
         default:
-            $sqlmain='Select s.`Date`, s.Posted from `'.$table.'`as s where TxnID='.$txnid;
+            $sqlmain='SELECT s.`Date`, s.Posted FROM `'.$table.'` s WHERE TxnID='.$txnid;
             $date='Date';
             break;
     }
@@ -50,6 +51,8 @@ function editOk($table,$txnid,$linkinfunction,$txntype){
     $stmt=$linkinfunction->query($sqlmain);
     $result=$stmt->fetch();
 	
+    if($stmt->rowCount()==0) { $condition=false; goto endreturn;}
+
     if ($txntype=='Out' or $txntype==4 or $txntype=='Repack' or $txntype==12){
         $condition=(($result['DateOUT']>$_SESSION['nb4']) AND ($result['Posted']==0) and date('Y', strtotime($result['DateOUT']))==$currentyr and ($result['BranchNo']==$_SESSION['bnum'])) ;
     } elseif ($txntype=='In' or $txntype==7){
@@ -59,6 +62,7 @@ function editOk($table,$txnid,$linkinfunction,$txntype){
         $condition=(strtotime($result[$date])>strtotime($closedate) and date('Y', strtotime($result[$date]))==$currentyr AND ($result['Posted']==0));
     }
 	
+    endreturn:
     if ($condition){
         return true;
     } else{
